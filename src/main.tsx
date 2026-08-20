1960d223e8876e94ba40260f93c89e72eb49bac6
Debar Flavien <flavien.debar@gmail.com>
2026-08-20 15:02:41 +0200

    feat: add 404 handler to support deep linking in SPA with history redirection

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from '@/app/app';
 import '@/styles/globals.css';
 
+const redirect = sessionStorage.getItem('kubesage-redirect');
+if (redirect) {
+  sessionStorage.removeItem('kubesage-redirect');
+  window.history.replaceState(null, '', `/kubesage-web${redirect}`);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
